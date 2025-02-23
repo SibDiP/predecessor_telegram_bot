67ca9024ec3990a84bd5e30a8cee144ba08ef870
Dmitrii Pivnev <tskdip@gmail.com>
2025-02-23 15:16:19 +0700

    Refactor and discription ps_to_pd_dataframe

--- ps_data_manager.py
+++ ps_data_manager.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-def ps_data_save(players_score : dict[str, float]) -> None:
-    print(players_score)
-    df = pd.DataFrame(data=players_score, index=[0]).T
-    print(df)
+def ps_to_pd_dataframe(players_score : dict[str, float]) -> pd.DataFrame:
+    """
+    Convert Player Score dictionary to pandas DataFrame with three
+    columns: id, Player, Player Score
+
+    return: pd.DataFrame
+    """
+    df = pd.DataFrame(data=list(players_score.items()), columns=
+    [
+        "Player",
+        "Player Score"],
+        )
+
+    logger.debug("\n%s", df.to_string())
+    logger.info("Create pandas DataFrame: Success")
+
+    return df
+
